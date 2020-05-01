3365c746378a5e2fb279ba6a864bd2edf1536ad1
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-05-01 15:27:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,13 +3,7 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  base_hash = {:railroads => {}}
- 
- 
- 
- 
- 
- base_hash = {:railroads => {:pieces => 4, :rent_in_dollars => {}, :names => {}}
+  base_hash = {:railroads => {:pieces => 4, :rent_in_dollars => {}, :names => {}}}
 end
 
 def monopoly_with_third_tier
